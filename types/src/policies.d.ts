de6bc0023474f40691eb06e26fbed84549dca1e7
Hamid Reza Shakibi <thisisnimdeveloper@gmail.com>
2026-02-25 15:11:28 +0330

    refactor: remove unused runPolicies function from policies type definitions

--- types/src/policies.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Runs policies sequentially.
- *
- * @param {Array} policies
- * @param {string} method
- * @param {any} params
- * @param {any} target
- */
-export function runPolicies(policies: any[], method: string, params: any, target: any): Promise<void>;
